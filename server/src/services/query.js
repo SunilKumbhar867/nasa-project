d31c700491485adbc813b115ea8998aef8bed2b7
SunilKumbhar867 <prajapati.sunil867@gmail.com>
2023-09-06 20:14:05 +0530

    command changes1

--- server/src/services/query.js
+++ server/src/services/query.js
@@ -2,9 +2,9 @@ const DEFAULT_PAGE_NUMBER = 1;
 const DEFAULT_PAGE_LIMIT = 0;
 
 async function getPagination(query) {
-  const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
+  const page = parseInt(query.page) || DEFAULT_PAGE_NUMBER;
 
-  const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
+  const limit = parseInt(query.limit) || DEFAULT_PAGE_LIMIT;
 
   const skip = (page - 1) * limit;
 
